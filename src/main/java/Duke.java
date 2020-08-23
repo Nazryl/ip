3b36e630b3992b57bc2f5382bdb8996392f3f6d7
Nazryl Lim <e0407382@u.nus.edu>
2020-08-23 22:43:55 +0800

    Update Duke.java Commands are added into list. Display of items in list.
    Update Allfather personality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
 
+    static final String COMMAND_LIST = "LIST";
     static final String COMMAND_BYE = "BYE";
+    static final int COMMAND_COUNT = 100;
 
     public static void printHello() {
         printLine();
@@ -13,7 +16,7 @@ public class Duke {
 
     public static void printBye() {
         printLine();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Begone!");
         printLine();
     }
 
@@ -23,12 +26,21 @@ public class Duke {
 
     public static void printEcho(String echo) {
         printLine();
-        System.out.println(echo);
+        System.out.println("added: " + echo);
+    }
+
+    public static void printList(String[] arrayList, int countList) {
+        printLine();
+        for (int i = 0; i < countList; i++) {
+            System.out.println(i + 1 + ". " + arrayList[i]);
+        }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String inputCommand;
+        String[] storeList = new String[COMMAND_COUNT];
+        int countList = 0;
 
         printHello();
 
@@ -36,14 +48,19 @@ public class Duke {
             inputCommand = in.nextLine();
 
             switch (inputCommand.toUpperCase()) {
-                case COMMAND_BYE:
+            case COMMAND_LIST:
+                printList(storeList, countList);
+                break;
+            case COMMAND_BYE:
                 printBye();
                 break loop;
             default:
+                storeList[countList] = inputCommand;
                 printEcho(inputCommand);
+                countList++;
             }
-            printLine();
 
+            printLine();
         }
 
     }
