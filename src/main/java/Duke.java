3fe49985a46b44ebca87b7233db2d95b7641bfb7
Nazryl Lim <e0407382@u.nus.edu>
2020-08-24 01:00:11 +0800

    Update Duke.java Add Task.java
    Removal of array method to list method on adding task for compatibility with Task.java
    Add checking of task and displaying of checking of task in list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,17 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
     static final String COMMAND_LIST = "LIST";
     static final String COMMAND_BYE = "BYE";
-    static final int COMMAND_COUNT = 100;
+    static final String COMMAND_DONE = "DONE";
+
+    static ArrayList<Task> taskList = new ArrayList<>();
 
     public static void printHello() {
         printLine();
-        System.out.println("Greetings Peasants! I am the Almighty Allfather!");
+        System.out.println("Greetings Peasants! I'm the Almighty Allfather!");
         System.out.println("What can I do for you today?");
         printLine();
     }
@@ -24,40 +26,56 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
-    public static void printEcho(String echo) {
+    public static void addTask(String echo) {
         printLine();
+        Task task = new Task(echo);
+        taskList.add(task);
         System.out.println("added: " + echo);
     }
 
-    public static void printList(String[] arrayList, int countList) {
+    public static void printList(ArrayList<Task> tasks) {
+        int index = 0;
+
         printLine();
-        for (int i = 0; i < countList; i++) {
-            System.out.println(i + 1 + ". " + arrayList[i]);
+        System.out.println("Here are the tasks in your list:");
+
+        for (Task task : tasks) {
+            System.out.println(String.format("%d.", ++index) + "[" + task.getStatusIcon() + "] " + task.description);
         }
+    }
+
+    public static void markTask(String inputCommand) {
+        int num = Integer.parseInt(inputCommand.split(" ")[1]);
+        Task task = taskList.get(num-1);
+        task.markAsDone();
+
+        printLine();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[" + task.getStatusIcon() + "] " + task.description);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String inputCommand;
-        String[] storeList = new String[COMMAND_COUNT];
-        int countList = 0;
 
         printHello();
 
         loop: while (true) {
             inputCommand = in.nextLine();
+            String firstCommand = inputCommand.split(" ")[0];
 
-            switch (inputCommand.toUpperCase()) {
+            switch (firstCommand.toUpperCase()) {
             case COMMAND_LIST:
-                printList(storeList, countList);
+                printList(taskList);
                 break;
             case COMMAND_BYE:
                 printBye();
                 break loop;
+            case COMMAND_DONE:
+                markTask(inputCommand);
+                break;
             default:
-                storeList[countList] = inputCommand;
-                printEcho(inputCommand);
-                countList++;
+                addTask(inputCommand);
             }
 
             printLine();
