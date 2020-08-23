f02b2d123f243aa9d8eb13cb47ee956d29879702
Nazryl Lim <e0407382@u.nus.edu>
2020-08-23 21:44:47 +0800

    Add features to Duke.java * Echo commands * Exit to exit program
    Duke will now be called Allfather.
    Update personality to Allfather.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,51 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+
+    static final String COMMAND_BYE = "BYE";
+
+    public static void printHello() {
+        printLine();
+        System.out.println("Greetings Peasants! I am the Almighty Allfather!");
+        System.out.println("What can I do for you today?");
+        printLine();
+    }
+
+    public static void printBye() {
+        printLine();
         System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printLine() {
         System.out.println("____________________________________________________________");
     }
+
+    public static void printEcho(String echo) {
+        printLine();
+        System.out.println(echo);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String inputCommand;
+
+        printHello();
+
+        loop: while (true) {
+            inputCommand = in.nextLine();
+
+            switch (inputCommand.toUpperCase()) {
+                case COMMAND_BYE:
+                printBye();
+                break loop;
+            default:
+                printEcho(inputCommand);
+            }
+            printLine();
+
+        }
+
+    }
+
 }
